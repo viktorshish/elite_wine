3a15834c95e7251a4cae1f5099516bc9996ee389
Viktor Shishkalov <viktorshish@gmail.com>
2023-09-22 12:01:08 +0300

    Создал Шаблон и подсчет времени

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+begin_year = datetime.date(year=1920)
+now_year = datetime.date.now()
+delta_year = now_year - begin_year
+print(delta_year)
+
+rendered_page = template.render
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
