24a5e442fb20e932f5e248aaad20b724b81cd89f
Viktor Shishkalov <viktorshish@gmail.com>
2023-09-23 16:10:38 +0300

    Добавил автоматическое вычисление года

--- main.py
+++ main.py
@@ -4,22 +4,43 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def get_delta_year():
+    begin_date = datetime.date(year=1920, month=1, day=1)
+    now_date = datetime.datetime.today()
+    delta_year = now_date.year - begin_date.year
+    return delta_year
 
-template = env.get_template('template.html')
 
-begin_year = datetime.date(year=1920)
-now_year = datetime.date.now()
-delta_year = now_year - begin_year
-print(delta_year)
+def get_year_declension(delta_year):
+    if delta_year % 100 in [11, 12, 13, 14]:
+        return 'лет'
+    elif delta_year % 10 == 1:
+        return 'год'
+    elif delta_year % 10 in [2, 3, 4]:
+        return 'года'
+    else:
+        return 'лет'
 
-rendered_page = template.render
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        delta_year=get_delta_year(),
+        year_declension=get_year_declension(get_delta_year())
+        )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
